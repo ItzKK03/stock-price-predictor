29e601f7f61e41b29b24b7bca6147d0218c38c83
Itz KK <kartikkant03102001@gmail.com>
2025-04-25 05:45:07 +0530

    Update nlp_sentiment.py

--- utils/nlp_sentiment.py
+++ utils/nlp_sentiment.py
@@ -1,24 +1,18 @@
-from transformers import pipeline
-import requests
-
-def fetch_news_headlines(ticker):
-    url = f"https://newsapi.org/v2/everything?q={ticker}&sortBy=publishedAt&apiKey=475f89bb10104be68933b0974559b5d4"
-    response = requests.get(url)
-    data = response.json()
-    headlines = [article['title'] for article in data.get('articles', [])][:10]
-    return headlines
+from textblob import TextBlob
 
 def analyze_sentiment(headlines):
-    classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
-    results = classifier(headlines)
-    
     sentiment_scores = []
     labels = []
-    
-    for result in results:
-        label = result['label']
-        score = result['score']
-        labels.append(label)
-        sentiment_scores.append(score if label == 'POSITIVE' else -score)
+
+    for headline in headlines:
+        blob = TextBlob(headline)
+        score = blob.sentiment.polarity
+        sentiment_scores.append(score)
+        if score > 0.1:
+            labels.append("POSITIVE")
+        elif score < -0.1:
+            labels.append("NEGATIVE")
+        else:
+            labels.append("NEUTRAL")
 
     return sentiment_scores, labels
